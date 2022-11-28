8906e713688de7515447ff143adabb95413fffe6
tracer-ics2019 <tracer@njuics.org>
2022-11-29 02:34:31 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:34:31 up  1:27,  1 user,  load average: 0.06, 0.10, 0.17 aabb868d85d42305e8e742c69a254d4eac97d052

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,7 +17,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-  struct {
+  union {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
@@ -31,6 +31,7 @@ typedef struct {
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t pc;
+
 
 } CPU_state;
 
