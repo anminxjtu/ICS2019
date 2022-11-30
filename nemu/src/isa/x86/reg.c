e19a335f23da89c45a0bfac037a732b4ba079fff
tracer-ics2019 <tracer@njuics.org>
2022-12-01 01:38:34 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:38:34 up  1:46,  1 user,  load average: 0.27, 0.32, 0.36 f05b706e5f6e03408bd6bf0fc296d9af65935e87

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -42,7 +42,9 @@ void reg_test() {
 
 void isa_reg_display() {
   printf("%%eax: 0x%x\t",cpu.eax);
-  printf("%%ax: 0x%x\n",cpu.eax & 0xffff);
+  printf("%%ax: 0x%x\t",cpu.eax & 0xffff);
+  printf("%%ah: 0x%x\t",(cpu.eax >> 16) & 0xff);
+  printf("%%al: 0x%x\n",cpu.eax & 0xff);
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
