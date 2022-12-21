7c85edc3491018dcc60961937ec171faa5a47119
tracer-ics2019 <tracer@njuics.org>
2022-12-21 22:02:29 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-53-generic #59~20.04.1-Ubuntu SMP Thu Oct 20 15:10:22 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:02:29 up  2:56,  1 user,  load average: 0.06, 0.14, 0.10 fe227afeb011fb76ce98d6ca15f4e59196c5341

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -88,11 +88,14 @@ static int cmd_x(char *args){
     }*/
     //printf("atoi_expr:%d\n",atoi(expr));
     int expr_value = 0;
+    char *expr_decimal = &expr[2];
+    printf("%s\n",expr_decimal);
+    
     sscanf(expr,"%x",&expr_value);
     printf("expr_value:%d\n",expr_value);
     // printf("atoi_N:%d\n",atoi(N));
     for(int i = 1;i <=atoi(N);i++){
-    	printf("0x%d -> 0x%d:",expr_value + 4,expr_value);
+    	printf("0x%d -> 0x%d:\t",expr_value + 4,expr_value);
     	printf("%08x\n",paddr_read(expr_value, 4));
     	expr_value = expr_value + 4;
     }
