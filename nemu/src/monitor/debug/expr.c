29e577b436255bcdca6efae4e1bc7d42e593974a
tracer-ics2019 <tracer@njuics.org>
2022-12-27 01:28:33 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:28:33 up 3 min,  1 user,  load average: 3.75, 1.65, 0.65 1f5fa7359330a0b8b57ee7cefca2ec381c1b70f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -21,7 +21,7 @@ static struct rule {
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
-  {"[\\d]", 0},
+  {"[\\[d]]", 0},
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"\\-", '-'},
