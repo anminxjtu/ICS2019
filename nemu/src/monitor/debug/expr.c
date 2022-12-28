9a2e9079fd79904bd931bf8cf5d2ea12f4ec05fa
tracer-ics2019 <tracer@njuics.org>
2022-12-28 16:24:52 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:24:52 up 18:50,  1 user,  load average: 0.05, 0.04, 0.06 d5c76eaac873e17780d8f613c3317ba9251b753c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -128,7 +128,7 @@ static bool make_token(char *e) {
   
   check_parentheses(0,nr_token);
   printf("begin evaling-------------\n");
-  eval(0,nr_token);
+  //eval(0,nr_token);
   //eval(0,0);
   printf("------------------\n");
 
