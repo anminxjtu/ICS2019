a5060b9512cde6d143195818b2a21e6fd359407f
tracer-ics2019 <tracer@njuics.org>
2022-12-28 17:15:49 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:15:49 up 19:41,  1 user,  load average: 0.09, 0.14, 0.11 6d1a713a6038c09fcd12615d4a763c8328e8670

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -130,6 +130,7 @@ static bool make_token(char *e) {
   printf("check_parentheses(0,nr_token-1):%d\n",check_parentheses(0,nr_token-1));
   printf("begin evaling-------------\n");
   eval(0,nr_token-1);
+  printf("eval(0,nr_token-1):%d\n",eval(0,nr_token-1));
   //eval(0,0);
   printf("------------------\n");
 
