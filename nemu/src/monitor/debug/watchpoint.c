f048a902dc69e57c27fe03b39786fa7e66d7787a
tracer-ics2019 <tracer@njuics.org>
2022-12-29 23:25:56 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:25:56 up 11:14,  1 user,  load average: 0.13, 0.11, 0.08 eb2f559d1569fc2267522cf3b9efa8cbe5877cff

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -52,12 +52,12 @@ int new_wp() {
 void free_wp(int no) {
   WP *wp = NULL; // p to no
   for (int i = 0; i < NR_WP; i ++) {
-    wp = wp_pool + i;
+    wp = wp_pool;
     printf("11111111111");
     printf("wp -> NO:%d",wp -> NO);
     if ((wp -> NO) == no)
       break;
-    
+    ;
       //assert(0);
   }
 
