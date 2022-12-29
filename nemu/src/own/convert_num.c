990a5194f4f9aaca67226fea3dc6498a60ce95ee
tracer-ics2019 <tracer@njuics.org>
2022-12-29 16:57:43 +0800

    >  compile 4122133001 min Linux ubuntu 5.15.0-56-generic #62~20.04.1-Ubuntu SMP Tue Nov 22 21:24:20 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:57:43 up  4:46,  1 user,  load average: 0.06, 0.05, 0.05 10393ca288f22a6ed4ebadb7578f948232944ab0

--- nemu/src/own/convert_num.c
+++ nemu/src/own/convert_num.c
@@ -1,4 +1,31 @@
-char* hex_dec(char *input)
+#include <string.h>
+#include <stdlib.h>
+
+int bit_convert(char *hex_bit);
+
+int hex2dec(char *input_hex)
 {
+   // input_hex = "0x1234";
+   int hex_len = strlen(input_hex);
+   int dec = 0;
+   int pow = 1;
+   for (int i = 0; i < hex_len-2; i++){
+     dec += pow * bit_convert(input_hex + hex_len -1 - i);
+     pow *= 16;
+   }
    return 0;
+}
+
+int bit_convert(char *hex_bit){
+  if (*hex_bit >= 48 && *hex_bit <=57) // ascii code 0:48; 9:57
+    return atoi(hex_bit);
+  switch (*hex_bit){
+    case 'a': return 10;
+    case 'b': return 11;
+    case 'c': return 12;
+    case 'd': return 13;
+    case 'e': return 14;
+    case 'f': return 15;
+  }
+  return -1;
 }
